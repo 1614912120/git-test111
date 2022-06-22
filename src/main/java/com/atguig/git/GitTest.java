a327c23cd59507f076d9b4a293dd469066f5befc
layne <Layne1614912120@qq.com>
2022-06-22 21:16:11 +0800

    2002/6/22 first commit

---
+++ src/main/java/com/atguig/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguig.git;
+
+/**
+ * @author 灰太狼
+ * @version 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!!!");
+    }
+}
