e6ac59f68692d958ad842c39824ed529b9c2161d
layne <Layne1614912120@qq.com>
2022-06-22 21:33:37 +0800

    2002/6/22 hot-fix commit1

--- src/main/java/com/atguig/git/GitTest.java
+++ src/main/java/com/atguig/git/GitTest.java
@@ -8,5 +8,6 @@ package com.atguig.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!!!");
+        System.out.println("hello git44");
     }
 }
