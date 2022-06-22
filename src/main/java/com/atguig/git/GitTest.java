935dbb83a3b15e6dfa37089e868f095d08ae2666
1614912120 <93957486+1614912120@users.noreply.github.com>
2022-06-22 22:12:03 +0800

    Update GitTest.java

--- src/main/java/com/atguig/git/GitTest.java
+++ src/main/java/com/atguig/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello git666");
         System.out.println("hello git7777");
         System.out.println("push test!!!");
+         System.out.println("pull test!!!");
     }
 }
